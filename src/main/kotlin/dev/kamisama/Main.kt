0c93df07141da6b2960cb7e51942c3f50f6be622
DemonHashira <v.logodazhki@outlook.com>
2025-12-05 20:55:21 +0200

    fix: handle exceptions in main function for improved error reporting

--- src/main/kotlin/dev/kamisama/Main.kt
+++ src/main/kotlin/dev/kamisama/Main.kt
@@ -1,12 +1,22 @@
 package dev.kamisama
 
 import com.github.ajalt.clikt.core.main
+import dev.kamisama.cli.Color
+import kotlin.system.exitProcess
 
 /**
  * Application entry point.
  */
 fun main(args: Array<String>) {
-    dev.kamisama.cli.CommandRouter
-        .build()
-        .main(args)
+    try {
+        dev.kamisama.cli.CommandRouter
+            .build()
+            .main(args)
+    } catch (e: IllegalArgumentException) {
+        System.err.println("${Color.red("fatal:")} ${e.message}")
+        exitProcess(1)
+    } catch (e: IllegalStateException) {
+        System.err.println("${Color.red("fatal:")} ${e.message}")
+        exitProcess(1)
+    }
 }
